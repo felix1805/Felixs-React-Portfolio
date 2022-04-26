f685e49c2334eb18f3e2e83337054aa1b80232c4
felix1805 <felix1805@gmail.com>
2022-04-26 18:41:13 -0400

    correct place holders

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -32,55 +32,67 @@ export default function Portfolio() {
       <h1>Portfolio Page</h1>
       <div style={styles.container}>
         <div className="card" style={styles.card}>
-          <img src="https://picsum.photos/300" style={styles.pic} width="105" alt="Card image cap" />
+          <a href='https://google.com'>
+            <img src="https://picsum.photos/300" style={styles.pic} width="105" alt="Card image cap" />
+          </a>
           <div className="card-body" style={styles.cardBody}>
             <h5 className="card-title">Project 1</h5>
             <p className="card-text">This is a description about the project.</p>
-            <a href="https://github.com/Sean-Duiser" className="btn btn-primary">View Here</a>
+            <a href="https://github.com/felix1805" className="btn btn-primary">View GitHub Repo</a>
           </div>
         </div>
         <div className="card" style={styles.card}>
-          <img src="https://picsum.photos/301" style={styles.pic}  width="106" alt="Card image cap" />
+        <a href='https://google.com'>
+          <img src="https://picsum.photos/301" style={styles.pic} width="106" alt="Card image cap" />
+          </a>
           <div className="card-body" style={styles.cardBody}>
             <h5 className="card-title">Project 2</h5>
             <p className="card-text">This is a description about the project.</p>
-            <a href="https://github.com/Sean-Duiser" className="btn btn-primary">View Here</a>
+            <a href="https://github.com/felix1805" className="btn btn-primary">View GitHub Repo</a>
           </div>
         </div>
         <div className="card" style={styles.card}>
-          <img src="https://picsum.photos/302" style={styles.pic}  width="107" alt="Card image cap" />
+        <a href='https://google.com'>
+          <img src="https://picsum.photos/302" style={styles.pic} width="107" alt="Card image cap" />
+          </a>
           <div className="card-body" style={styles.cardBody}>
             <h5 className="card-title">Project 3</h5>
             <p className="card-text">This is a description about the project.</p>
-            <a href="https://github.com/Sean-Duiser" className="btn btn-primary">View Here</a>
+            <a href="https://github.com/felix1805" className="btn btn-primary">View GitHub Repo</a>
           </div>
         </div>
         <div className="card" style={styles.card}>
-          <img src="https://picsum.photos/303" style={styles.pic}  width="108" alt="Card image cap" />
+        <a href='https://google.com'>
+          <img src="https://picsum.photos/303" style={styles.pic} width="108" alt="Card image cap" />
+          </a>
           <div className="card-body" style={styles.cardBody}>
             <h5 className="card-title">Project 4</h5>
             <p className="card-text">This is a description about the project.</p>
-            <a href="https://github.com/Sean-Duiser" className="btn btn-primary">View Here</a>
+            <a href="https://github.com/felix1805" className="btn btn-primary">View GitHub Repo</a>
           </div>
         </div>
         <div className="card" style={styles.card}>
-          <img src="https://picsum.photos/304" style={styles.pic}  width="109" alt="Card image cap" />
+        <a href='https://google.com'>
+          <img src="https://picsum.photos/304" style={styles.pic} width="109" alt="Card image cap" />
+          </a>
           <div className="card-body" style={styles.cardBody}>
             <h5 className="card-title">Project 5</h5>
             <p className="card-text">This is a description about the project.</p>
-            <a href="https://github.com/Sean-Duiser" className="btn btn-primary">View Here</a>
+            <a href="https://github.com/felix1805" className="btn btn-primary">View GitHub Repo</a>
           </div>
         </div>
         <div className="card" style={styles.card}>
-          <img src="https://picsum.photos/305" style={styles.pic}  width="110" alt="Card image cap" />
+        <a href='https://google.com'>
+          <img src="https://picsum.photos/305" style={styles.pic} width="110" alt="Card image cap" />
+          </a>
           <div className="card-body" style={styles.cardBody}>
             <h5 className="card-title">Project 6</h5>
             <p className="card-text">This is a description about the project.</p>
-            <a href="https://github.com/Sean-Duiser" className="btn btn-primary">View Here</a>
+            <a href="https://github.com/felix1805" className="btn btn-primary">View GitHub Repo</a>
           </div>
         </div>
       </div>
-        <Footer />
+      <Footer />
     </div>
   );
 }
